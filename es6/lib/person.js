f0b48e3d8fc87b7b5858f1a3361337f7d80a972d
chenjiahau0214 <Ivan_Chen@dlink.com.tw>
2016-03-25 13:46:01 +0800

    Init

---
+++ es6/lib/person.js
@@ -0,0 +1,5 @@
+"use strict";
+
+var q = 98;
+var myVariable = q + " bottles of beer on the wall, " + q + " bottles of beer.";
+//# sourceMappingURL=person.js.map
---
+++ es6/src/person.es6
@@ -0,0 +1,2 @@
+let q = 98;
+let myVariable = `${q} bottles of beer on the wall, ${q} bottles of beer.`;
